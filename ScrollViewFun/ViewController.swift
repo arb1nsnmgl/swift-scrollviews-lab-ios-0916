8e8cf8b34b17ef1f81b5f610c8931112246e70e7
arb1nsnmgl <arvinsanmiguel@gmail.com>
2016-10-17 16:00:20 -0400

    Completed

--- ScrollViewFun/ViewController.swift
+++ ScrollViewFun/ViewController.swift
@@ -10,18 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var mainScrollView: UIScrollView!
+    
+    var images = [UIImage]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+     
+    mainScrollView.frame = view.frame
+        
+    images = [#imageLiteral(resourceName: "Anger"), #imageLiteral(resourceName: "Disgust"), #imageLiteral(resourceName: "Fear"), #imageLiteral(resourceName: "Sadness"), #imageLiteral(resourceName: "Joy")]
+        
+        for i in 0..<images.count {
+            
+            let imageView = UIImageView()
+            imageView.image = images[i]
+            imageView.contentMode = .scaleToFill
+            let xPos = self.view.frame.width * CGFloat(i)
+            imageView.frame = CGRect(x: xPos, y: 0.0, width: self.mainScrollView.frame.width, height: self.mainScrollView.frame.height)
+            
+            mainScrollView.contentSize.width = mainScrollView.frame.width * CGFloat(i + 1)
+            mainScrollView.addSubview(imageView)
+            
+            
+            
+        }
         
         
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+   
 
 }
 
